604963b7be689139ae81a53db6dcb666843906d1
fahdfady <fahd.fady212@gmail.com>
2025-06-16 06:30:32 +0300

    handle connection and shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::Write;
-use std::net::{TcpListener, TcpStream};
+use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
@@ -10,17 +10,38 @@ fn main() {
     let listener = TcpListener::bind(format!("0.0.0.0:{}", PORT)).expect("could not bind listener");
     // let (mut stream, _socket) = listener.accept().unwrap();
 
-    let mut shutdown = Arc::new(Mutex::new(false));
+    let shutdown = Arc::new(Mutex::new(false));
 
     // check if the server has connections for the last minute
     // let mut : bool = false;
 
     let mut connections = 0;
-    thread::Builder::new()
+
+    let server_handler = thread::Builder::new()
+        .name(format!("server"))
+        .spawn({
+            println!("Starting server ..");
+            println!("━━(￣ー￣*|||━━");
+            let shutdown = Arc::clone(&shutdown);
+            let mut i = 0;
+            move || {
+                while !*shutdown.lock().unwrap() {
+                    i += 1;
+
+                    let (stream, socket) = listener.accept().unwrap();
+
+                    handle_connection(i, stream, socket);
+                }
+            }
+        })
+        .expect("failed to connect to a thread");
+
+    let counter_handler = thread::Builder::new()
         .name("please-connect".to_string())
         .spawn({
+            let shutdown = Arc::clone(&shutdown);
             move || {
-                loop {
+                while !*shutdown.lock().unwrap() {
                     println!("Hey my guy, this server is awaiting for connections!");
                     thread::sleep(Duration::from_secs(5));
                 }
@@ -28,31 +49,32 @@ fn main() {
         })
         .unwrap();
 
-    let server_handler = thread::Builder::new()
-        .name(format!("server"))
-        .spawn({
-            println!("Starting server ..");
-            println!("━━(￣ー￣*|||━━");
-            let shutdown = shutdown.clone();
-            let mut i = 0;
-            move || {
-                while !*shutdown.lock().unwrap() {
-                    i += 1;
+    let stream = server_handler.join().unwrap();
 
-                    let (stream, socket) = listener.accept().unwrap();
-                    println!("received connection from conn-{}", i)
-                }
-            }
-        })
-        .expect("failed to connect to a thread");
+    {
+        // lock aqcuired
+        let mut guard = shutdown.lock().unwrap();
+        *guard = true;
+        // lock dropped
+    }
+    println!("Shutdown initiated");
 
-    let sream = server_handler.join().unwrap();
+    counter_handler.join().unwrap();
+    println!("Shutdown completed");
 }
 
-// fn handle_connection(i: i32, mut stream: TcpStream) -> JoinHandle<()> {
-//     let join_handle = stream.write(b"Hello Fahd!").expect("failed to write");
+fn handle_connection(i: i32, mut stream: TcpStream, socket: SocketAddr) -> JoinHandle<()> {
+    println!("connected to conn-{}", i);
 
-//     println!("connected to conn-{}", i);
-
-//     join_handle
-// }
+    thread::Builder::new()
+        .name(format!("conn-{}", i))
+        .spawn(move || {
+            loop {
+                stream
+                    .write(b"Hello, Fahd!")
+                    .expect("could not write to stream");
+                thread::sleep(Duration::from_secs(2));
+            }
+        })
+        .unwrap()
+}
