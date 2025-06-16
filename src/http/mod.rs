180088b1d7eaf274ea74739781e702c12a5d42c7
fahdfady <fahd.fady212@gmail.com>
2025-06-16 19:43:11 +0300

    print out http requests to a file to avoid filling out the command line

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::{BufReader, prelude::*};
 use std::net::TcpStream;
 
@@ -5,16 +6,32 @@ use crate::http::utils::request_is_http;
 
 mod utils;
 
-pub fn read_http_request(stream: TcpStream) {
+pub fn read_http_request(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let buf_reader_clone = BufReader::new(&stream);
     let http_request: Vec<String> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    request_is_http(http_request.clone());
+    if request_is_http(http_request.clone()) {
+        // maybe put that writing to a file logic into a separate thread?
+        std::fs::create_dir("tmp").expect_err("couldn't create dir tmp");
 
-    println!("Request: {:#?}", http_request);
+        let already_existing_requests =
+            std::fs::read_to_string("tmp/requests.txt").unwrap_or("".to_string());
+
+        std::fs::write(
+            "tmp/requests.txt",
+            format!(
+                "{}\n Request: {:#?}",
+                already_existing_requests, http_request
+            ),
+        )
+        .expect("could not write requests to tmp/requests.txt");
+
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
