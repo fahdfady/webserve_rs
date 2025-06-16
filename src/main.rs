c1a683be3fdba1fd7b709a07edfb35900143f86f
fahdfady <fahd.fady212@gmail.com>
2025-06-16 12:21:08 +0300

    refactor connection handling and add HTTP request reading and really basic validation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::io::Write;
+use std::io::{BufReader, prelude::*};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 use std::time::Duration;
+use std::{fs, vec};
 
 use color_print::cprintln;
 
@@ -17,7 +18,7 @@ fn main() {
     // check if the server has connections for the last minute
     // let mut : bool = false;
 
-    let mut connections = 0;
+    let connections = Arc::new(Mutex::new(0));
 
     let server_handler = thread::Builder::new()
         .name(format!("server"))
@@ -25,14 +26,25 @@ fn main() {
             println!("Starting server ..");
             println!("━━(￣ー￣*|||━━");
             let shutdown = Arc::clone(&shutdown);
+            let connections = Arc::clone(&connections);
+            // todo: debug the number of connections here.
+
             let mut i = 0;
             move || {
                 while !*shutdown.lock().unwrap() {
                     i += 1;
+                    *connections.lock().unwrap() += 1;
+                    // todo: debug the number of connections here.
 
                     let (stream, socket) = listener.accept().unwrap();
 
-                    handle_connection(i, stream, socket, Arc::clone(&shutdown));
+                    handle_connection(
+                        i,
+                        stream,
+                        socket,
+                        Arc::clone(&shutdown),
+                        Arc::clone(&connections),
+                    );
                 }
             }
         })
@@ -42,11 +54,16 @@ fn main() {
         .name("please-connect".to_string())
         .spawn({
             let shutdown = Arc::clone(&shutdown);
+            let connections = Arc::clone(&connections);
+
+            // todo: debug the number of connections here.
             move || {
                 while !*shutdown.lock().unwrap() {
+                    // while *connections.lock().unwrap() > 0 {
                     println!("Hey my guy, this server is awaiting for connections!");
                     thread::sleep(Duration::from_secs(5));
                 }
+                // }
             }
         })
         .unwrap();
@@ -70,7 +87,10 @@ fn handle_connection(
     mut stream: TcpStream,
     socket: SocketAddr,
     shutdown: Arc<Mutex<bool>>,
+    connections: Arc<Mutex<i32>>,
 ) -> JoinHandle<()> {
+    // todo: debug the number of connections here.
+    // todo: minus one connection for the disconnected connections
     let connection_id = format!("conn-{}", i);
 
     println!("connected to {}", connection_id);
@@ -80,7 +100,12 @@ fn handle_connection(
         .name(connection_id.clone())
         .spawn(move || {
             while !*shutdown.lock().unwrap() {
-                if let Err(e) = stream.write(b"Hello, Fahd!") {
+                //  FIX: this write to stream does not work anymore
+                //      because of the read_request function.
+
+                read_http_request(stream.try_clone().expect("cloning stream failed"));
+
+                if let Err(e) = { stream.write(b"Hello, Fahd!") } {
                     println!("Error writing to stream: {}, closing connection", e);
                     break;
                 }
@@ -92,4 +117,33 @@ fn handle_connection(
             cprintln!("<r>connection <bold>{}</bold> closed</>", connection_id);
         })
         .unwrap()
+}
+
+fn read_http_request(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let buf_reader_clone = BufReader::new(&stream);
+    let http_request: Vec<String> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    request_is_http(http_request.clone());
+
+    println!("Request: {:#?}", http_request);
+}
+
+fn request_is_http(mut request: Vec<String>) -> bool {
+    let http_request_start_line_rules = vec![
+        "GET", "POST", "TRACE", "PUT", "HEAD", "CONNECT", "DELETE", "PATCH",
+    ];
+
+    for rule in http_request_start_line_rules {
+        if request[0].contains(&rule.to_owned()) {
+            println!("hahahaha that is an HTTP request congrats !!! ╰(*°▽°*)╯");
+            continue;
+        };
+    }
+
+    true
 }
